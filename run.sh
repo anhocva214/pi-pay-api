fdc5b3ca2f060096e9382855b432e4bc823f8d88
N/A <N/A>
2024-06-26 11:33:45 +0700

    update

--- run.sh
+++ run.sh
@@ -2,5 +2,5 @@
 docker image rm pipay_api:latest
 docker build -t pipay_api:latest .
 docker rm -f pipay_api
-docker run -it -d -p 8000:8181 --name pipay_api pipay_api:latest
+docker run -it -d -p 8400:8181 --name pipay_api pipay_api:latest
 # docker run -it -d --name pipay_api --network apps -e LETSENCRYPT_EMAIL="anhocva@gmail.com" pipay_api:latest
